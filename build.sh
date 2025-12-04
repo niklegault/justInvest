57729fc0aa3e777aefbbd172d0316b613ec80cc3
Nik Legault <nik.legault.12@gmail.com>
2025-12-04 16:17:12 -0500

    Fixed build script to not be interrupted when downloading dependencies

--- build.sh
+++ build.sh
@@ -5,13 +5,11 @@ BUILD_DIR="build"
 LIB_DIR="${BUILD_DIR}/lib"
 CLASSES_DIR="${BUILD_DIR}/classes"
 
-# Primary Dependency: spring-security-crypto (required by PasswordHashing.java)
 SPRING_VERSION="6.2.0"
 SPRING_ARTIFACT="spring-security-crypto"
 SPRING_FILE="${SPRING_ARTIFACT}-${SPRING_VERSION}.jar"
 SPRING_URL="https://repo1.maven.org/maven2/org/springframework/security/${SPRING_ARTIFACT}/${SPRING_VERSION}/${SPRING_FILE}"
 
-# Transitive Dependency: commons-logging (required by spring-security-crypto)
 LOGGING_VERSION="1.3.0"
 LOGGING_ARTIFACT="commons-logging"
 LOGGING_FILE="${LOGGING_ARTIFACT}-${LOGGING_VERSION}.jar"
@@ -24,34 +22,31 @@ MANIFEST_FILE="${BUILD_DIR}/MANIFEST.MF"
 
 echo "--- 1. Cleaning up previous build artifacts ---"
 rm -rf ${BUILD_DIR}
-rm -f ${OUTPUT_JAR}
 mkdir ${BUILD_DIR}
 mkdir ${LIB_DIR}
 mkdir ${CLASSES_DIR}
 
-echo "--- 2. Downloading Dependencies into ${LIB_DIR} ---"
 
-# Download spring-security-crypto
-echo "Downloading ${SPRING_FILE}..."
-if wget -L -o "${LIB_DIR}/${SPRING_FILE}" "${SPRING_URL}"; then
-    echo "${SPRING_FILE} downloaded successfully."
-else
-    echo "ERROR: Failed to download ${SPRING_FILE}. Exiting."
-    exit 1
-fi
+echo "--- 2. Downloading Dependencies into ${LIB_DIR} using wget ---"
 
-# Download commons-logging
-echo "Downloading ${LOGGING_FILE}..."
-if wget -L -o "${LIB_DIR}/${LOGGING_FILE}" "${LOGGING_URL}"; then
-    echo "${LOGGING_FILE} downloaded successfully."
-else
-    echo "ERROR: Failed to download ${LOGGING_FILE}. Exiting."
-    exit 1
-fi
+download_file() {
+    local url=$1
+    local output_path=$2
+    local filename=$3
+    echo "Downloading ${filename}..."
+    if wget -O "${output_path}" -c --tries=5 "${url}"; then
+        echo "${filename} downloaded successfully."
+    else
+        echo "ERROR: Failed to download ${filename}. This usually indicates a network issue or an invalid URL. Exiting."
+        exit 1
+    fi
+}
 
-# Set the combined classpath variable
+download_file "${SPRING_URL}" "${LIB_DIR}/${SPRING_FILE}" "${SPRING_FILE}"
+
+download_file "${LOGGING_URL}" "${LIB_DIR}/${LOGGING_FILE}" "${LOGGING_FILE}"
+
 DEPENDENCY_PATH="${LIB_DIR}/${SPRING_FILE}:${LIB_DIR}/${LOGGING_FILE}"
-
 
 echo "--- 3. Creating Manifest File (${MANIFEST_FILE}) ---"
 echo "Main-Class: ${MAIN_CLASS}" > ${MANIFEST_FILE}
@@ -85,6 +80,7 @@ cp "${LIB_DIR}/${SPRING_FILE}" ${BUILD_DIR}/
 cp "${LIB_DIR}/${LOGGING_FILE}" ${BUILD_DIR}/
 
 rm -rf ${LIB_DIR}
+
 
 echo "========================================"
 echo "BUILD COMPLETE! Files are in the '${BUILD_DIR}' directory."
